5f00b0999a081edffca3beeb7f02c6dc4ced46e3
Noah Stolk <noah.stolk1997@gmail.com>
2018-09-29 23:22:48 +0200

    no message

--- NetBase/Utils/ConsoleUtils.cs
+++ NetBase/Utils/ConsoleUtils.cs
@@ -4,16 +4,15 @@ namespace NetBase.Utils
 {
 	public static class ConsoleUtils
 	{
-		public static void WriteLineColor(string message, ConsoleColor color)
-		{
-			Console.ForegroundColor = color;
-			Console.WriteLine(message);
-		}
-
 		public static void WriteColor(string message, ConsoleColor color)
 		{
 			Console.ForegroundColor = color;
 			Console.Write(message);
+		}
+
+		public static void WriteLineColor(string message, ConsoleColor color)
+		{
+			WriteColor($"{message}\n", color);
 		}
 	}
 }
