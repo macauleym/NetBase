a3c6684215febc8399680ca192dbb791070aa08b
Noah Stolk <noah.stolk1997@gmail.com>
2019-06-09 12:10:49 +0200

    Add Lerp method to MathUtils.

--- NetBase/Utils/MathUtils.cs
+++ NetBase/Utils/MathUtils.cs
@@ -4,6 +4,11 @@ namespace NetBase.Utils
 {
 	public static class MathUtils
 	{
+		public static float Lerp(float value1, float value2, float amount)
+		{
+			return value1 + (value2 - value1) * amount;
+		}
+
 		public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
 		{
 			return (value.CompareTo(min) < 0) ? min : (value.CompareTo(max) > 0) ? max : value;
